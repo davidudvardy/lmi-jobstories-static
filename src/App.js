520ccf168a1255f7ed57c628a0d8945db70836a2
David Udvardy <udvardy@gmail.com>
2018-10-10 02:08:02 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import Card from './Card/Card'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Work in progress...</p>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/">Job Stories</a>
+          <input className="form-control form-control-dark w-100" type="text" placeholder="Search" />
+        </nav>
+        <div className="container-fluid">
+          <div className="row">
+            <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+              <div className="sidebar-sticky">
+                <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">Product</h6>
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Category</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Category</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Category</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+              <h2>Job Stories</h2>
+              <Card/>
+            </main>
+          </div>
+        </div>
       </div>
     );
   }
