f323ae12d3e7a9e296055525322f8e1a1c68a4d3
David Udvardy <udvardy@gmail.com>
2018-10-10 20:52:00 +0200

    filter working

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/">Job Stories</a>
-          <input className="form-control form-control-dark w-100" type="text" placeholder="Search" />
+          <input className="form-control form-control-dark w-100" id="filter" type="text" placeholder="Search job stories..." />
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap">
               <a className="nav-link" href="#">Sign out</a>
--- src/components/JobStoryList.js
+++ src/components/JobStoryList.js
@@ -6,18 +6,38 @@ class JobStoryList extends Component {
         super(props);
 
         this.state = {
+            jobStoryFilter: "",
+            originalJobStoryList: props.data.jobs,
             filteredJobStoryList: props.data.jobs,
         };
+    }
+    
+    componentDidMount() {
+        document.getElementById("filter").onchange = this.handleChange;
+    }
+
+    handleChange = (event) => {
+        let list = this.state.originalJobStoryList;
+        let pattern = event.target.value.toLowerCase();
+        list = list.filter(function(job) {
+            return job.context.toLowerCase().search(pattern) !== -1 || 
+                job.motivation.toLowerCase().search(pattern) !== -1 || 
+                job.outcome.toLowerCase().search(pattern) !== -1;
+        });
+        this.setState({
+            jobStoryFilter: event.target.value,
+            filteredJobStoryList: list
+        });
     }
 
     render() {
         return (
             this.state.filteredJobStoryList.map(job => (
-            <div>
-                <Card text={job.context} type="context"/>
-                <Card text={job.motivation} type="motivation"/>
-                <Card text={job.outcome} type="outcome"/>
-            </div>
+                <div>
+                    <Card text={job.context} type="context"/>
+                    <Card text={job.motivation} type="motivation"/>
+                    <Card text={job.outcome} type="outcome"/>
+                </div>
             ))
         )
     }
