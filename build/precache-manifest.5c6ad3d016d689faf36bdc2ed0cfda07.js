ec3c5f0deb692a6e6710c767cfb57a03cf3dc62a
David Udvardy <udvardy@gmail.com>
2018-10-13 21:20:32 +0200

    added `build` again, after gitignore fixed

---
+++ build/precache-manifest.5c6ad3d016d689faf36bdc2ed0cfda07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a8da67385457c84f859",
+    "url": "/static/js/main.1a8da673.chunk.js"
+  },
+  {
+    "revision": "10aa0aa5aad2c2c41187",
+    "url": "/static/js/1.10aa0aa5.chunk.js"
+  },
+  {
+    "revision": "1a8da67385457c84f859",
+    "url": "/static/css/main.885f6dff.chunk.css"
+  },
+  {
+    "revision": "891d3ff12ac67c9a983036d1def99742",
+    "url": "/index.html"
+  }
+];
