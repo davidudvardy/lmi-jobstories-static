342723e295b00cbc0f2e99e58c8ca6fd79b86a98
David Udvardy <udvardy@gmail.com>
2018-10-17 16:20:18 +0200

    snapshot before refactor

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,7 +4,7 @@ function Card(props) {
     return (
         <div className={"list-group-item list-group-item-action flex-row align-items-start " + props.type.toLowerCase()} style={{maxWidth: 20 + 'rem'}}>
             <small style={{textTransform: 'capitalize'}}>{props.type}</small>
-            <p className="mb-1">{props.text || 'Empty card'}</p>
+            <p className="mb-1">{props.text}</p>
         </div>
     )
 }
